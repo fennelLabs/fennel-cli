beddcddc1567a15d290f350e234addb7a651e26c
Sean Batzel <romulus108@protonmail.com>
2022-01-19 22:48:23 -0500

    Started off with dependencies and module structure.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fennel-cli
+```
+virtualenv -p python3 venv
+
+source venv/bin/activate
+
+pip install -r requirements.txt
+```
---
+++ src/crypto/aes.py
@@ -0,0 +1,18 @@
+def generate_key():
+    pass
+
+
+def encrypt():
+    pass
+
+
+def decrypt():
+    pass
+
+
+def sign():
+    pass
+
+
+def verify():
+    pass
