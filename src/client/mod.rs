517c7d6e653e8d0acc62ce3e6623b8f0260542d4
Sean Batzel <romulus108@protonmail.com>
2022-03-23 14:05:58 -0400

    Readability improvements.

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -33,6 +33,7 @@ pub async fn handle_connection(
         panic!("server packet signature failed to verify");
     }
     if server_packet.command == [0] {
+        // Submit Identity
         let r = submit_identity(identity_db, server_packet).await;
         if r != [0] {
             panic!("identity failed to commit.");
@@ -41,6 +42,7 @@ pub async fn handle_connection(
         println!("sent");
         stream.read_exact(&mut server_response_code).await?;
     } else if server_packet.command == [3] {
+        // Retrieve Identity
         stream.write_all(&server_packet.encode()).await?;
         println!("sent");
         let mut return_packet_binary = [0; 3112];
@@ -53,6 +55,7 @@ pub async fn handle_connection(
         }
         stream.read_exact(&mut server_response_code).await?;
     } else if server_packet.command == [1] {
+        // Send Message
         let r = send_message(message_db, server_packet).await;
         if r != [0] {
             panic!("message failed to commit.");
@@ -61,6 +64,7 @@ pub async fn handle_connection(
         println!("sent");
         stream.read_exact(&mut server_response_code).await?;
     } else if server_packet.command == [2] {
+        // Receive all new messages
         stream.write_all(&server_packet.encode()).await?;
         println!("sent");
         let mut response: Vec<[u8; 3111]> = Vec::new();
