9b05e7ebaf50524b4902540002398b5273916415
Sean Batzel <romulus108@protonmail.com>
2022-05-25 20:50:55 -0400

    This RPC should only need encode/decode/sign/verify. We can add Whiteflag features as well.

--- src/fennel_rpc/service.rs
+++ src/fennel_rpc/service.rs
@@ -1,11 +1,10 @@
-use fennel_lib::{get_identity_database_handle, get_message_database_handle, FennelServerPacket};
+use fennel_lib::{get_identity_database_handle};
 use jsonrpsee::core::{async_trait, Error};
-use tokio::net::TcpStream;
 
 use super::traits::FennelRPCServer;
 use super::types::{FennelFingerprint, FennelPublicKeyBytes, FennelSignature};
 use crate::client::{
-    handle_connection, handle_decrypt, handle_encrypt, handle_generate_keypair, handle_sign,
+    handle_decrypt, handle_encrypt, handle_generate_keypair, handle_sign,
     handle_verify,
 };
 
@@ -15,103 +14,6 @@ pub struct FennelRPCService;
 impl FennelRPCServer<FennelFingerprint, FennelSignature, FennelPublicKeyBytes>
     for FennelRPCService
 {
-    async fn create_identity(
-        &self,
-        fingerprint: FennelFingerprint,
-        signature: FennelSignature,
-        public_key_bytes: FennelPublicKeyBytes,
-    ) -> Result<Vec<u8>, Error> {
-        let packet = FennelServerPacket {
-            command: [0; 1],
-            identity: [0; 4],
-            fingerprint,
-            message: [0; 512],
-            signature: signature.try_into().unwrap(),
-            public_key: public_key_bytes.try_into().unwrap(),
-            recipient: [0; 4],
-            message_type: [0; 1],
-        };
-        let listener: TcpStream = TcpStream::connect("127.0.0.1:7878").await?;
-        let identity_db = get_identity_database_handle();
-        let message_db = get_message_database_handle();
-        handle_connection(identity_db, message_db, listener, packet).await?;
-        Ok("Identity created successfully".as_bytes().to_vec())
-    }
-
-    async fn retrieve_identity(
-        &self,
-        identity_id: u32,
-        fingerprint: FennelFingerprint,
-        signature: FennelSignature,
-        public_key_bytes: FennelPublicKeyBytes,
-    ) -> Result<Vec<u8>, Error> {
-        let packet = FennelServerPacket {
-            command: [3; 1],
-            identity: identity_id.to_ne_bytes(),
-            fingerprint,
-            message: [0; 512],
-            signature: signature.try_into().unwrap(),
-            public_key: public_key_bytes.try_into().unwrap(),
-            recipient: [0; 4],
-            message_type: [0; 1],
-        };
-        let listener: TcpStream = TcpStream::connect("127.0.0.1:7878").await?;
-        let identity_db = get_identity_database_handle();
-        let message_db = get_message_database_handle();
-        handle_connection(identity_db, message_db, listener, packet).await?;
-        Ok("Identity information updated".as_bytes().to_vec())
-    }
-
-    async fn send_message(
-        &self,
-        sender_id: u32,
-        recipient_id: u32,
-        ciphertext: Vec<u8>,
-        fingerprint: FennelFingerprint,
-        signature: FennelSignature,
-        public_key_bytes: FennelPublicKeyBytes,
-    ) -> Result<Vec<u8>, Error> {
-        let packet = FennelServerPacket {
-            command: [1; 1],
-            identity: sender_id.to_ne_bytes(),
-            fingerprint,
-            message: ciphertext.to_owned().try_into().unwrap(),
-            signature: signature.try_into().unwrap(),
-            public_key: public_key_bytes.try_into().unwrap(),
-            recipient: recipient_id.to_ne_bytes(),
-            message_type: [1; 1],
-        };
-        let listener: TcpStream = TcpStream::connect("127.0.0.1:7878").await?;
-        let identity_db = get_identity_database_handle();
-        let message_db = get_message_database_handle();
-        handle_connection(identity_db, message_db, listener, packet).await?;
-        Ok("Message sent".as_bytes().to_vec())
-    }
-
-    async fn get_messages(
-        &self,
-        id: u32,
-        fingerprint: FennelFingerprint,
-        signature: FennelSignature,
-        public_key_bytes: FennelPublicKeyBytes,
-    ) -> Result<Vec<u8>, Error> {
-        let packet = FennelServerPacket {
-            command: [2; 1],
-            identity: id.to_ne_bytes(),
-            fingerprint,
-            message: [0; 512],
-            signature: signature.try_into().unwrap(),
-            public_key: public_key_bytes.try_into().unwrap(),
-            recipient: [0; 4],
-            message_type: [0; 1],
-        };
-        let listener: TcpStream = TcpStream::connect("127.0.0.1:7878").await?;
-        let identity_db = get_identity_database_handle();
-        let message_db = get_message_database_handle();
-        handle_connection(identity_db, message_db, listener, packet).await?;
-        Ok("Messages received".as_bytes().to_vec())
-    }
-
     async fn encrypt(&self, identity: u32, plaintext: Vec<u8>) -> Result<Vec<u8>, Error> {
         let identity_db = get_identity_database_handle();
         Ok(handle_encrypt(
