1274775eb25a9f8f06fd99bab9e68e1f8f43f928
Sean Batzel <romulus108@protonmail.com>
2022-06-02 16:38:17 -0400

    This should work for hosting the RPC on a websocket.

--- src/fennel_rpc/mod.rs
+++ src/fennel_rpc/mod.rs
@@ -2,19 +2,25 @@ mod service;
 mod traits;
 mod types;
 
-use jsonrpsee::http_server::{HttpServerBuilder, HttpServerHandle};
+use jsonrpsee::ws_server::WsServerBuilder;
 use std::net::SocketAddr;
 
 use service::FennelRPCService;
 
 use self::traits::FennelRPCServer;
 
-pub async fn start_rpc() -> anyhow::Result<(SocketAddr, HttpServerHandle)> {
-    let server = HttpServerBuilder::default()
+#[allow(unreachable_code)]
+pub async fn start_rpc() -> anyhow::Result<()> {
+    let server = WsServerBuilder::default()
         .build("127.0.0.1:9030".parse::<SocketAddr>()?)
         .await?;
 
-    let addr = server.local_addr()?;
-    let server_handle = server.start(FennelRPCService.into_rpc())?;
-    Ok((addr, server_handle))
+    server.local_addr()?;
+    server.start(FennelRPCService.into_rpc())?;
+
+    loop {
+        tokio::time::sleep(std::time::Duration::from_millis(50)).await;
+    }
+
+    Ok(())
 }
