5d6a56bb69dc0805b073a2a12ed61488fcbee97a
Sean Batzel <romulus108@protonmail.com>
2022-10-05 17:40:49 -0400

    Added help content for each command option.

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -10,13 +10,19 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
+    /// RSA encrypts a given string with a known public key for the given identity.
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Encrypt { plaintext: String, identity: u32 },
+
+    /// RSA decrypts ciphertext encrypted for the current private key
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Decrypt { ciphertext: String },
 
+    /// Begins a Diffie-Hellman handshake
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     GenerateEncryptionChannel {},
+
+    /// Finishes a Diffie-Hellman handshake
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     AcceptEncryptionChannel {
         identity_id: u32,
@@ -31,12 +37,15 @@ pub enum Commands {
     //     recipient_id: u32,
     // },
 
+    /// Encrypts a string with an AES secret
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     AESEncrypt {
         secret: String,
         public_key: String,
         plaintext: String,
     },
+
+    /// Decrypts an AES-encrypted string
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     AESDecrypt {
         secret: String,
@@ -44,12 +53,15 @@ pub enum Commands {
         ciphertext: String,
     },
 
+    /// Verifies a message given a signature for an identity
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Verify {
         message: String,
         signature: String,
         identity: u32,
     },
+
+    /// RSA signs the given message
     #[clap(setting(AppSettings::ArgRequiredElseHelp))]
     Sign { message: String },
 
@@ -72,6 +84,7 @@ pub enum Commands {
     // #[clap()]
     // RetrieveIdentities {},
 
+    /// Runs a WebSocket RPC exposing crypto functions to parallel applications
     #[clap()]
     StartRPC {},
 }
