8c85be4db3b4114c33cbb94ce22b2c7e0a37a9bf
Sean Batzel <romulus108@protonmail.com>
2022-10-08 14:44:01 -0400

    New function adding to IPFS by content.

--- src/ipfs/main.rs
+++ src/ipfs/main.rs
@@ -1,25 +1,32 @@
 #![allow(deprecated)]
 
 mod command;
+use std::fs;
+
 use clap::Parser;
 use command::{Cli, Commands};
 
-use fennel_lib::{add_file, del_file, get_file};
+use fennel_lib::{add_content_by_local_path, add_content_by_string, delete_content_by_cid, get_content_by_cid};
 
 pub fn main() {
     let args = Cli::parse();
 
     match &args.command {
+        Commands::AddString { file_content } => {
+            let result = add_content_by_string(file_content).unwrap();
+            fs::remove_file("upload.txt").unwrap();
+            println!("{}", result);
+        }
         Commands::AddFile { filename } => {
-            let result = add_file(filename);
+            let result = add_content_by_local_path(filename).unwrap();
             println!("{}", result);
         }
         Commands::GetFile { cid } => {
-            let result = get_file(cid);
-            println!("{}", result)
+            let result = get_content_by_cid(cid).unwrap();
+            println!("{}", result);
         }
         Commands::DeleteFile { cid } => {
-            if del_file(cid) {
+            if delete_content_by_cid(cid).unwrap() {
                 println!("File deleted successfully");
             } else {
                 println!("File failed to delete");
