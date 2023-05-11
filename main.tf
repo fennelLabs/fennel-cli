57ea0ea1bd3a21ab77288da72807fec3fab37f8f
Ed Hertzog <edhertzog@gmail.com>
2023-05-10 20:51:20 -0400

    Update start script

--- main.tf
+++ main.tf
@@ -39,8 +39,10 @@ resource "google_compute_instance" "fennel-cli" {
     sudo apt-get update
     sudo apt-get install -y docker.io
     gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-    docker pull us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-cli:latest
     docker run -dit -p 9031:9031 --name fennel-cli us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-cli:latest
+    docker exec -it fennel-cli sh
+    cd /app/target/release/build
+    cargo run --bin fennel-cli start-api
   EOF  
 
  metadata = {
