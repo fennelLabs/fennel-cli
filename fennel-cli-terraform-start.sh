ee6d3c507e2c4d63a29a571ac42bf9ba8adf6259
Sean Batzel <romulus108@protonmail.com>
2023-05-11 21:26:28 -0400

    This should correctly run the API on launch.

--- fennel-cli-terraform-start.sh
+++ fennel-cli-terraform-start.sh
@@ -3,6 +3,4 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
 docker run -dit -p 9031:9031 --name fennel-cli us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-cli:latest
-docker exec -it fennel-cli sh
-cd /app/target/release/build
-cargo run --bin fennel-cli start-api
+docker exec -it fennel-cli /app/target/release/build/fennel-cli start-api
