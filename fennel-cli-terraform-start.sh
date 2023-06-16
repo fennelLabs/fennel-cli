edb36ed2cbca3baf84eb44da9daa4861810d4bec
Sean Batzel <romulus108@protonmail.com>
2023-06-15 21:32:33 -0400

    Fixed a docker run problem.

--- fennel-cli-terraform-start.sh
+++ fennel-cli-terraform-start.sh
@@ -2,5 +2,5 @@
 sudo apt-get update
 sudo apt-get install -y docker.io
 gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin us-east1-docker.pkg.dev
-docker run -dit -p 9031:9031 --name fennel-cli us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-cli:latest
-docker exec -it fennel-cli /app/target/release/build/fennel-cli start-api
+docker run -dt -p 9031:9031 --name fennel-cli us-east1-docker.pkg.dev/whiteflag-0/fennel-docker-registry/fennel-cli:latest
+docker exec fennel-cli /app/target/release/build/fennel-cli start-api
